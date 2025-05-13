75ad795ef22214ef17b3f955259d5f1d25fcdef6
saikumarbommuluri3069 <saikumar319jdqa@gmail.com>
2025-05-13 08:32:08 +0530

    ContactPageDetailsChange

--- src/main/java/webPages/MyInfoClass.java
+++ src/main/java/webPages/MyInfoClass.java
@@ -14,9 +14,12 @@ import org.openqa.selenium.By;
     {
         private static By contactDetailsButton = By.xpath(".//*[text()='Contact Details']");
         private static By savebutton = By.xpath("(.//*[@type='submit'])[1]");
-
+        private static By streetNameField = By.xpath("(.//*[@class='oxd-input oxd-input--active'])[2]");
+        private static By  cityNameField  = By.xpath("(.//*[@class='oxd-input oxd-input--active'])[4]");
         public void contactDetailsSection() {
             clickTheElement(contactDetailsButton);
+
+            enterInput(streetNameField, "8BJBDUU88-Parkstreet");
             clickTheElement(savebutton);
         }
 
